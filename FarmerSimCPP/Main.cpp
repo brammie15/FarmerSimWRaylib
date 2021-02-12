c579522cbf2b8fabf57700f2e743ed14f0b5cbf0
brammie15 <brammieverhulst@gmail.com>
2021-02-12 13:37:38 +0100

    idk

--- FarmerSimCPP/Main.cpp
+++ FarmerSimCPP/Main.cpp
@@ -2,13 +2,25 @@
 #include "raymath.h"
 #include "GroundTile.h"
 #include "SpriteUtils.h"
-//#include "PlantTile.h"
+
 
 const Rectangle ZERO_REC{ 0,0,0,0 };
 const Vector2 ZERO_VEC{ 0,0 };
-enum ItemType { empty = 0, wheatSeed = 1 };
-enum ToolType { hand = 0, hoe = 1};
+enum ItemType {empty = 0,hoe = 1, wheatSeed = 2};
+class Item {
+	public:
+		bool IsUseable;
+		ItemType type;
+		int amount;
+		Item();
+		Item(bool isUseable, ItemType type, int amount) {
+			this->IsUseable = isUseable;
+			this->type = type;
+			this->amount = amount;
+		}
 
+		
+};
 
 
 //void Plant(PlantTypes type, int selec, GroundTile a[],Texture2D tex[]) {
@@ -38,7 +50,6 @@ int main()
 	//--------------------------------------------------------------------------------------
 	const int screenWidth = 1600;
 	const int screenHeight = 800;
-
 	InitWindow(screenWidth, screenHeight, "Farmer Sim");
 
 	Texture2D FarmerTex = LoadTexture("Textures/Farmer.png");
@@ -53,15 +64,20 @@ int main()
 	Texture2D SelectionBoxTex = LoadTexture("Textures/SelectionBox.png");
 	Texture2D PlantTex[] = { WheatTex };
 
+	Item items[3];
+	for (int i = 0; i < 3; i++)
+	{
+		items[i] = Item(false, empty, 1);
+	}
+
 	Sprite Farmer = createSprite(FarmerTex, Vector2{ 32, 32 }, 2, 1, Vector2{ 0, 0 }, 0, Vector2{ 0, screenHeight - (160 * 3) });
 	Sprite SelectionBox = createSprite(SelectionTex, Vector2{ 16, 16 }, 1, 1, Vector2{ 0, 0 }, 0, Vector2{ 0, screenHeight - 160 });
-	Sprite CurrentTool = createSprite(HoeTex, Vector2{ 16, 16 }, 1, 1, Vector2{ 0, 0 }, 0, Vector2{ 10, 10});
 	Sprite SelectionBoxItem = createSprite(SelectionBoxTex, Vector2{ 22,22 }, 1, 1, Vector2{ 0, 0 }, 0, Vector2{ 0, 0 });
 
 	int selectionIndex = 0;
-	bool Facing = false;
 	bool Selecting = false;
 
+	
 	Rectangle SkyRec = { 0, 0, (float)SkyTex.width, (float)SkyTex.height };
 	Rectangle SkyRecScale = { 0, 0, 1600, 800 + 160 };
 	//TODO: Improve ground System ALOT
@@ -76,41 +92,40 @@ int main()
 		tile.sprite.maxFrame = 1;
 		tile.sprite.origin = Vector2{ 0,0 };
 
-		/*PlantTile p = PlantTile();
-		p.sprite = createSprite(WheatTex, Vector2{ 16,16 }, 8, 1, Vector2{ 0,0 }, 0, Vector2{ 100,100 });
-		p.sprite.pos.x = a * 160;
-		p.sprite.pos.y = screenHeight - (160 * 2);
-		tile.CurrentPlant = p;*/
 		GroundTiles[a] = tile;
 	}
-
+	
 	SetTargetFPS(60);               
 	while (!WindowShouldClose())    
 	{
 		//Possible TODO: add/implemnt input system (Optional)
 		if (IsKeyDown(KEY_D)) {
 			if (Farmer.pos.x < screenWidth - 160) {
-				Farmer.pos.x += 4.0f;
+				if (IsKeyDown(KEY_LEFT_SHIFT)) {
+					Farmer.pos.x += 6.0f;
+				}
+				else {
+					Farmer.pos.x += 4.0f;
+				}
 			}
-			Facing = false;
+			Farmer.frame = 0;
 		}
 		if (IsKeyDown(KEY_A)) {
 			if (Farmer.pos.x > 0) {
-				Farmer.pos.x -= 4.0f;
+				if (IsKeyDown(KEY_LEFT_SHIFT)) {
+					Farmer.pos.x -= 6.0f;
+				}
+				else {
+					Farmer.pos.x -= 4.0f;
+				}
 			}
-			Facing = true;
+			Farmer.frame = 1;
 		}
 		if (IsKeyDown(KEY_S)) {
 			Selecting = true;
 		}
 		else {
 			Selecting = false;
-		}
-		if (Facing) {
-			Farmer.frame = 1;
-		}
-		else {
-			Farmer.frame = 0;
 		}
 		if (IsKeyDown(KEY_SPACE) && Selecting == true) {
 			GroundTiles[selectionIndex].Plant(Wheat, PlantTex);
@@ -122,14 +137,13 @@ int main()
 		ClearBackground(RAYWHITE);
 		DrawTexturePro(SkyTex, SkyRec, SkyRecScale, Vector2{ 0, 0 }, 0, RAYWHITE);
 		drawSprite(&SelectionBoxItem, 0, 6, false);
-		drawSprite(&CurrentTool, 0, 7, false);
+		//drawSprite(&inv.CurrentItemSprite, 0, 7, false);
 		drawSprite(&Farmer, 0, 10, false);
 		for (int x = 0; x < 10; x++) {
 			GroundTile t = GroundTiles[x];
 			drawSprite(&t.sprite, 0, 10, false);
 			drawSprite(&t.CurrentPlant.sprite,0,10,false);       
 		}
-
 		if (Selecting) {
 			selectionIndex = round(Remap(Farmer.pos.x, 0, screenWidth, 0, 10));
 			SelectionBox.pos.x = selectionIndex * 160;
